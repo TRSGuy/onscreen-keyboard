1018ee37e6d61487645681ad09512fdc86182465
Kim Karlsson <vorap@voxelnet.io>
2017-09-20 11:33:47 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,86 @@
+var keyboard = {
+    "se": [
+	{
+	    "pos": [0, 0],
+	    "values": {
+		"normal": "1",
+		"shift": [true, "!"],
+		"alt": [false],
+		"cap": [false]
+	    }
+	},
+	{
+	    "pos": [0, 1],
+	    "values": {
+		"normal": "2",
+		"shift": [true, '"'],
+		"alt": [true, "@"],
+		"cap": [false]
+	    }
+	},
+	{
+	    "pos": [0, 2],
+	    "values": {
+		"normal": "3",
+		"shift": [true, "#"],
+		"alt": [true, "£"],
+		"cap": [false]
+	    }
+	},
+	{
+	    "pos": [0, 3],
+	    "values": {
+		"normal": "4",
+		"shift": [true, "¤"],
+		"alt": [true, "$"],
+		"caps": [false]
+	    }
+	},
+	{
+	    "pos": [0, 4],
+	    "values": {
+		"normal": "5",
+		"shift": [true, "%"],
+		"alt": [false],
+		"caps": [false]
+	    }
+	},
+	{
+	    "pos" : [0, 5],
+	    "values": {
+		"normal": "6",
+		"shift": [true, "&"],
+		"alt": [false],
+		"caps": [false]
+	    }
+	},
+	{
+	    "pos": [0, 6],
+	    "values": {
+		"normal": "7",
+		"shift": [true, "/"],
+		"alt":  [true, "{"]
+		"caps": [false]
+	    }
+	},
+	{
+	    "pos": [0, 7],
+	    "values": {
+		"normal": "8",
+		"shift": [true, "("],
+		"alt": [true, "["],
+		"caps": [false]
+	    }
+	},
+	{
+	    "pos": [0, 8],
+	    "values": {
+		"normal": "9",
+		"shift": [true, ")"],
+		"alt": [true, "]"],
+		"caps": [false]
+	    }
+	},
+	{}
+    ]
+}
